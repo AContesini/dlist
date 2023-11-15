4a0b5131ea34677b90f2ae3139c1d873c94ed9d5
Adriano Contesini <104787052+AContesini@users.noreply.github.com>
2023-11-14 22:51:38 -0300

    Get game by id

--- src/main/java/com/adriec/dslist/services/GameService.java
+++ src/main/java/com/adriec/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.adriec.dslist.dto.GameDTO;
 import com.adriec.dslist.dto.GameMinDTO;
 import com.adriec.dslist.entities.Game;
 import com.adriec.dslist.repositories.GameRepository;
@@ -14,6 +16,13 @@ public class GameService {
 	
 	@Autowired
 	private GameRepository gameRepository;
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
 	
 	public List<GameMinDTO> findAll(){
 		 List<Game> result = gameRepository.findAll();
